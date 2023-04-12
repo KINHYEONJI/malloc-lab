77d3f6c369e644dca85334fb4159d4be0c291937
KINHYEONJI <hyeonji0718@gmail.com>
2023-04-12 04:54:29 +0000

    feat : segregated-fit 방식 매크로 및 전역변수 생성

---
+++ mms/segregated-fit.c
@@ -0,0 +1,64 @@
+/*
+ * mm-naive.c - The fastest, least memory-efficient malloc package.
+ *
+ * In this naive approach, a block is allocated by simply incrementing
+ * the brk pointer.  A block is pure payload. There are no headers or
+ * footers.  Blocks are never coalesced or reused. Realloc is
+ * implemented directly using mm_malloc and mm_free.
+ *
+ * NOTE TO STUDENTS: Replace this header comment with your own header
+ * comment that gives a high level description of your solution.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "mm.h"
+#include "memlib.h"
+
+/*********************************************************
+ * NOTE TO STUDENTS: Before you do anything else, please
+ * provide your team information in the following struct.
+ ********************************************************/
+team_t team = {
+    /* Team name */
+    "jungle6_week06_6",
+    /* First member’s full name */
+    "KimHyeonji",
+    /* First member’s email address */
+    "hyeonji0718 @gmail.com",
+    /* Second member’s full name (leave blank if none) */
+    "",
+    /* Second member’s email address (leave blank if none) */
+    ""};
+
+#define WSIZE sizeof(void *) // pointer의 크기 (32bit -> WSIZE = 4 / 64bit -> WSIZE = 8)
+#define DSIZE (2 * WSIZE)    // double word size
+#define MINIMUM 8
+#define CHUNKSIZE (1 << 12)
+#define LISTLIMIT 20
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+#define PACK(size, alloc) ((size) | (alloc))
+
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+#define HDRP(ptr) ((char *)(ptr)-WSIZE)
+#define FTRP(ptr) ((char *)(ptr) + GET_SIZE(HDRP(ptr)) - DSIZE)
+
+#define NEXT_BLKP(ptr) ((char *)(ptr) + GET_SIZE(((char *)(ptr)-WSIZE)))
+#define PREV_BLKP(ptr) ((char *)(ptr)-GET_SIZE(((char *)(ptr)-DSIZE)))
+
+// segreagated-fit 방식에서 추가된 메크로 (prev, next 보다는 predecessor, successor이 직관적)
+#define PREC_FREEP(ptr) (*(void **)(ptr))
+#define SUCC_FREEP(ptr) (*(void **)(ptr + WSIZE))
+
+static char *heap_listp;
+static void *seg_list[LISTLIMIT];
